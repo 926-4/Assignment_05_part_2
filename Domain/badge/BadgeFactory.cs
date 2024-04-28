fc4bb9a177cba46b72e1d541f90057ca51aad47b
SzilagyiBotond <ancelotti156@gmail.com>
2024-04-28 21:16:30 +0300

    BadgeFactory

---
+++ Domain/badge/BadgeFactory.cs
@@ -0,0 +1,35 @@
+﻿using System.Drawing;
+
+namespace UBB_SE_2024_Team_42.Domain.Badge
+{
+    internal class BadgeFactory
+    {
+        public Badge Instance = new ();
+        public BadgeFactory NewBadge()
+        {
+            Instance = new ();
+            return this;
+        }
+        public BadgeFactory SetName(string name)
+        {
+            Instance.Name = name;
+            return this;
+        }
+        public BadgeFactory SetDescription(string description)
+        {
+            Instance.Description = description;
+            return this;
+        }
+        public BadgeFactory SetImage(Image image)
+        {
+            Instance.Image = image;
+            return this;
+        }
+        public Badge Get()
+        {
+            Badge returnValue = Instance;
+            Instance = new ();
+            return returnValue;
+        }
+    }
+}
