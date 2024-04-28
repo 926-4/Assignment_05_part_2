28cf9cd98877fc3a447a86dbd85b650eb70b5a07
SzilagyiBotond <ancelotti156@gmail.com>
2024-04-28 21:09:48 +0300

    NotificationFactory

---
+++ Domain/notification/NotificationFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBB_SE_2024_Team_42.Domain.Notification
+{
+    internal class NotificationFactory
+    {
+        public Notification Instance = new ();
+
+        public NotificationFactory NewNotification()
+        {
+            Instance = new ();
+            return this;
+        }
+        public NotificationFactory SetText(string text)
+        {
+            Instance.Text = text;
+            return this;
+        }
+        public NotificationFactory SetPostId(long postId)
+        {
+            Instance.PostID = postId;
+            return this;
+        }
+        public NotificationFactory SetBadgeId(long badgeId)
+        {
+            Instance.BadgeID = badgeId;
+            return this;
+        }
+        public Notification Get()
+        {
+            Notification returnValue = Instance;
+            Instance = new ();
+            return returnValue;
+        }
+    }
+}
