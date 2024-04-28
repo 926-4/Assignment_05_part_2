d2d939003f62d5eed46af4219167b3f9d3cce8dd
SzilagyiBotond <ancelotti156@gmail.com>
2024-04-28 20:52:54 +0300

    TagFactory

---
+++ Domain/tag/TagFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBB_SE_2024_Team_42.Domain.Tag
+{
+    internal class TagFactory
+    {
+        public Tag Instance = new ();
+
+        public TagFactory NewTag()
+        {
+            Instance = new ();
+            return this;
+        }
+
+        public TagFactory SetName(string name)
+        {
+            Instance.Name = name;
+            return this;
+        }
+
+        public TagFactory Get()
+        {
+            Tag returnValue = Instance;
+            Instance = new ();
+            return this;
+        }
+    }
+}
